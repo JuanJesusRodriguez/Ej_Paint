aee038cb223a1ad25ce3dbf098d54922be502714
Sergio Alvaré Peláez <sergioalvare@hotmail.com>
2022-09-07 09:09:54 +0200

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "square.h"
+using namespace std;
+
+Point Square::getPointFinal()
+{
+    return this->pointFinal;
+}
+
+Point Square::getPointInitial()
+{
+    return this->pointInitial;
+}
