3472ceb3e384e48af131c668fefef648a7239040
Juan Jesús <108931438+JuanJesusRodriguez@users.noreply.github.com>
2022-09-07 08:21:04 +0200

    Add files via upload

---
+++ color.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "Color.h"
+using namespace std;
+
+Color::Color(string color){this->color = color;}
+string Color::getColor(){return this->color;}
---
+++ color.h
@@ -0,0 +1,16 @@
+#include<map>
+#ifndef __COLOR__HPP
+#define __COLOR__HPP
+using namespace std;
+class Color{
+
+    private:
+    string color;
+
+    public:
+    Color(string color); 
+    string getColor();
+
+};
+
+#endif // __COLOR__HPP
