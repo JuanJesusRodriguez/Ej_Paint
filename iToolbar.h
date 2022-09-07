ac9c4187eefd445e0218b4cda3a567c44fee03a2
Sergio Alvaré Peláez <sergioalvare@hotmail.com>
2022-09-07 09:31:53 +0200

    Create iToolbar.h

---
+++ iToolbar.h
@@ -0,0 +1,11 @@
+#include "shape.h"
+#include "color.h"
+#ifndef __ITOOLBAR__HPP
+#define __ITOOLBAR__HPP
+
+class IToolbar {
+ public:
+  virtual Shape* CreateShape() = 0;
+  virtual Color* PickColor() = 0;
+};
+#endif
