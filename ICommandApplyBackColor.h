e5a5d24bd794b4d736372546e5ec3857ae085e09
Juan Jesús <108931438+JuanJesusRodriguez@users.noreply.github.com>
2022-09-07 14:38:28 +0200

    Revision

---
+++ ICommandApplyBackColor.h
@@ -0,0 +1,12 @@
+#include"color.h"
+#include"shape.h"
+#ifndef __ICOMMANDAPLYBACKCOLOR__H
+#define __ICOMMANDAPLYBACKCOLOR__H
+
+class ICommandApplyBackColor{
+
+    public:
+    virtual void applyBackColor(Shape* shape,Color color) = 0;
+};
+
+#endif // __ICOMMANDAPLYBACKCOLOR__H
---
+++ ICommandApplyForeColor.h
@@ -0,0 +1,12 @@
+#include"color.h"
+#include"shape.h"
+#ifndef __ICOMMANDAPLYFORECOLOR__H
+#define __ICOMMANDAPLYFORECOLOR__H
+
+class ICommandApplyForeColor{
+
+    public:
+    virtual void applyForeColor(Shape* shape,Color color) = 0;
+};
+
+#endif // __ICOMMANDAPLYFORECOLOR__H
