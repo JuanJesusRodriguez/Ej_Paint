b0ff68796652d0dfe681293c1a22b6cd54e39a9f
Juan Jesús <108931438+JuanJesusRodriguez@users.noreply.github.com>
2022-09-07 10:08:38 +0200

    Update color.h

--- color.h
+++ color.h
@@ -3,14 +3,11 @@
 #define __COLOR__HPP
 using namespace std;
 class Color{
-
     private:
     string color;
 
     public:
     Color(string color); 
     string getColor();
-
 };
-
 #endif // __COLOR__HPP
