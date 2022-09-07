b5514ac6cbe727cd6b03750546c45226ec7ee552
Juan Jesús <108931438+JuanJesusRodriguez@users.noreply.github.com>
2022-09-07 09:34:39 +0200

    Revisions

---
+++ ISelectFigura.h
@@ -0,0 +1,12 @@
+#include "shape.h"
+#ifndef __ISELECTSHAPE__HPP
+#define __ISELECTSHAPE__HPP
+
+class IselectShape{
+
+    public:
+    virtual void selectShape(Shape Shape) = 0;
+};
+
+
+#endif // __ISELECTSHAPE__HPP
