ea600f556e275005e9fb577a3f8b44178080540b
Juan Jesús <108931438+JuanJesusRodriguez@users.noreply.github.com>
2022-09-07 09:28:31 +0200

    Revisions

---
+++ ISelectColor.h
@@ -0,0 +1,12 @@
+#include "color.h"
+#ifndef __ISELECTCOLOR__HPP
+#define __ISELECTCOLOR__HPP
+
+class IselectColor{
+
+    public:
+    virtual void selectColor(Color color) = 0;
+};
+
+
+#endif // __ISELECTCOLOR__HPP
