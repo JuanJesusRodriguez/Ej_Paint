8bfce34ff900d79ad87a5cc4283b3fd5a3a06d3e
Juan Jesús <108931438+JuanJesusRodriguez@users.noreply.github.com>
2022-09-07 14:37:43 +0200

    Update ICommandList.h

--- ICommandList.h
+++ ICommandList.h
@@ -1,12 +1,12 @@
 #include "point.h"
 #include <string>
 using namespace std;
-#ifndef __ILIST__HPP
-#define __ILIST__HPP
+#ifndef __ICOMMANDLIST__HPP
+#define __ICOMMANDLIST__HPP
 
-    class Ilist{
+    class ICommandlist{
         public:
         virtual void list(string type,Point point) = 0;
     };
 
-#endif /* __ILIST__HPP */
+#endif /* __ICOMMANDLIST__HPP */
