640f683493ae7ca9397c59a47e22014f00f3ca09
Sergio Alvaré Peláez <sergioalvare@hotmail.com>
2022-09-08 11:08:30 +0200

    Added

---
+++ RemoveAll.h
@@ -0,0 +1,13 @@
+#include "IRemoveAll.h"
+
+#ifndef __REMOVEALL__HPP
+#define __REMOVEALL__HPP
+
+template <typename T>
+class IRemoveAll
+{
+    public:
+        void removeAll(std::list<T>){};
+};
+
+#endif
