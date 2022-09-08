046e3a570e9e9adb3c5fefa420b242ad76052338
Sergio Alvaré Peláez <sergioalvare@hotmail.com>
2022-09-08 08:58:24 +0200

    Edit IFileRead
    Change in order to return a list conatining the lines of the file. This will clearly difference this function from the content of IFileOpen.h

--- IFileRead.h
+++ IFileRead.h
@@ -1,12 +1,13 @@
 #include <fstream>
 #include <string>
+#include <list>
 
 #ifndef __IFILEREAD__HPP
 #define __IFILEREAD__HPP
 
 class IFileRead{
     public:
-        virtual std::fstream fileRead(std::string fileFullPath) = 0;
+        virtual std::list<std::string> fileRead(std::fstream) = 0;
 };
 
 #endif
