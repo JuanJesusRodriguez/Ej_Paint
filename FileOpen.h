60ef39fe914817378376db3ae1885a43129ad8fc
Sergio Alvaré Peláez <sergioalvare@hotmail.com>
2022-09-08 10:25:42 +0200

    Create FileOpen.h

---
+++ FileOpen.h
@@ -0,0 +1,12 @@
+#include "IFileOpen.h"
+
+#ifndef __FILEOPEN__HPP
+#define __FILEOPEN__HPP
+
+class FileOpen: public IFileOpen
+{
+    public:
+        std::fstream fileOpen(std::string path){};
+};
+
+#endif
