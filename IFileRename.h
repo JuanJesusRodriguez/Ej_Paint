8c8fa91c991c67357394326a7d6ca8b7d52ef778
LuMaIg <96819263+LuMaIg@users.noreply.github.com>
2022-09-08 11:01:30 +0200

    Update IFileRename.h
    IFileRename.h corrections

--- IFileRename.h
+++ IFileRename.h
@@ -1,12 +1,13 @@
 #include <fstream>
 #include <string>
+#include "ICurrentFilePath.h"
 
 #ifndef __IFILERENAMER__HPP
 #define __IFILERENAMER__HPP
 
 class IFileRename {
     public:
-        virtual void FileRename(std::string fullPath, std::string currentFileName, std::string newFileName) = 0;
+        virtual void fileRename(ICurrentFilePath* currentFilePath, std::string newFileName) = 0;
 };
 
 #endif
